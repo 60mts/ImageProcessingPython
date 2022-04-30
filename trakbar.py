201cb0e7f8ae6a9a3c832f8450540cfd465a2044
ᴍᴜʜᴀᴍᴍᴇᴅ ᴛᴀʏʏɪʙ Şᴀʜɪɴ <muhammedtayyibsahin@yandex.com>
2022-04-30 20:43:34 +0300

    master

--- trakbar.py
+++ trakbar.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 
 def nothing(x):
+    
     pass
 
     img=np.zeros((550,550,3),np.uint8)
@@ -13,21 +14,23 @@ def nothing(x):
     
     switch='0 : KAPA \n1:AC'
     cv2.createTrackbar(switch, 'image',0,1,nothing)
-
+    
     while(1):
         cv2.imshow('image',img)
         k=cv2.waitKey(1)&0xFFFF
-        if k==27:
-            break
         
+        if k==27:
+                break
         
         r=cv2.getTrackbarPos('R','image')
         g=cv2.getTrackbarPos('G','image')
         b=cv2.getTrackbarPos('B','image')
         s=cv2.getTrackbarPos('switch','image')
-        if s==0:
+            
+        if s==0: 
             img[:]=0
+            
         else:
-            img[:]=[b,g,r]
+            img[:][b,g,r]
 
         cv2.destroyAllWindows()        
